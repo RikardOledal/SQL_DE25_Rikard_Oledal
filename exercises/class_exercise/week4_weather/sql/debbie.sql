019a28db848fad84837128cd51f43b5a0b82bc35
RIKARD OLEDAL <rikardoledal@gmail.com>
2025-11-25 14:15:41 +0100

    Debbie

--- exercises/class_exercise/week4_weather/sql/debbie.sql
+++ exercises/class_exercise/week4_weather/sql/debbie.sql
@@ -27,3 +27,39 @@ ORDER BY
   State;
 
 -- Task 4
+
+SELECT 
+  date_part('year', to_timestamp(sunriseTime)) AS year,
+  date_part('month', to_timestamp(sunriseTime)) AS month,
+  to_timestamp(MAX(sunriseTime)),
+  to_timestamp(MAX(sunsetTime)),
+  round(max(sunsetTime - sunriseTime)/3600) AS sunlight_time
+FROM
+  staging.weather
+WHERE
+  "Country/Region" = 'Sweden'
+GROUP BY
+  year,
+  month
+ORDER BY
+  month;
+
+-- Task 5
+
+SELECT
+  *
+FROM
+  staging.weather
+WHERE
+  "Country/Region" = 'Sweden';
+
+-- Task 6
+
+SELECT
+  to_timestamp(windGustTime) AT TIME ZONE 'Europe/Stockholm' AS most_windy_timestamp,
+  date_part('hour', to_timestamp(windGustTime)) AS most_windy_hour,
+  CONCAT('Its dangerous to use the crane at kl.',most_windy_hour)
+FROM
+  staging.weather
+WHERE
+  "Country/Region" = 'Sweden';
